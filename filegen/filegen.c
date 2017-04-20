c7cd1311f2d465e7f329627ae4d6606f531c627f
xa-vez <omarchavezmiranda@gmail.com>
2017-04-20 16:19:59 +0200

    initial import

---
+++ filegen/filegen.c
@@ -0,0 +1,16 @@
+/**
+ * @file
+ **/
+#include <stdio.h>
+
+/**
+ * @brief main entry point
+ * @return true
+ **/
+int filegen(void) {
+
+	printf("filegen\n");
+
+	return 1;
+}
+
---
+++ hello/main.c
@@ -0,0 +1,22 @@
+/**
+ * @file
+ **/
+#include <stdio.h>
+
+
+/**
+ * @brief main entry point
+ * @return true
+ **/
+int main(void) {
+
+	printf("hello world\n");
+
+	printf("size of char: %d\n", (int) sizeof(char));
+	printf("size of short: %d\n", (int) sizeof(short));
+	printf("size of int: %d\n", (int) sizeof(int));
+	printf("size of long: %d\n", (int) sizeof(long));
+	printf("size of double: %d\n", (int) sizeof(double));
+
+	return 1;
+}
