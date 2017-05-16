cca6bb9524314f8262d3477c23f6319d80384b55
Omar Chavez <o.chavez@kerlink.fr>
2017-05-16 16:51:06 +0200

    hello world application

--- hello/hello.c
+++ hello/hello.c
@@ -1,6 +1,6 @@
 /**
  * @file main.c
- * @brief prints the architecture size
+ * @brief prints the hello world string
  * $Id: $
  **/
 #include <stdio.h>
@@ -11,11 +11,6 @@
  **/
 int main(void) {
 
-	printf("size of char: %d\n", (int) sizeof(char));
-	printf("size of short: %d\n", (int) sizeof(short));
-	printf("size of int: %d\n", (int) sizeof(int));
-	printf("size of long: %d\n", (int) sizeof(long));
-	printf("size of double: %d\n", (int) sizeof(double));
-
+	printf("hello world\n");
 	return 1;
 }
