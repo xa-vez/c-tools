1aab02e3e90a4e38aa38e504c66eafb467c8f876
Omar Chavez <ochavez@esoftthings.com>
2019-03-20 15:27:20 +0100

    [ble] repport files creation

--- app/unix/ble-dialog-semiconductor/src/main.c
+++ app/unix/ble-dialog-semiconductor/src/main.c
@@ -12,16 +12,8 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <unistd.h>
-//#include <sys/socket.h>
-//#include <bluetooth/bluetooth.h>
-//#include <bluetooth/rfcomm.h>
-//#include <bluetooth/hci.h>
-//#include <bluetooth/hci_lib.h>
 #include <assert.h>
 #include "debug.h"
-
-//#include <glib.h>
-//#include <iostream>
 #include "gattlib.h"
 #include "main.h"
 
@@ -63,6 +55,13 @@ static void usage(char *argv[]) {
 	printf("%s <device_address> <read|write> <uuid> [<hex-value-to-write>]\n", argv[0]);
 }
 
+struct ble_multisensors{
+	float temperature;
+	float humidity;
+	float pressure;
+} ;
+
+static struct ble_multisensors device = {0};
 
 /**
  * @brief App Initialization
@@ -75,20 +74,30 @@ int main(int argc, char *argv[]) {
 	gatt_connection_t* connection;
 	unsigned char device_config[] = {0x0A, 0x08, 0x03, 0x06, 0x03, 0x06, 0x00, 0x02, 0x0A, 0x00, 0x01, 0x00, 0x00, 0x00, 0x05};
 	unsigned char device_request[] = {0x01 };
+	int value;
 
-	fprintf(stderr, "1. Initilializing communication with : %s \n", DIALOG_MULTISENSOR);
+	TRACE_DEBUG("================================\n");
+	TRACE_DEBUG("======= BLE APPLICATION =======\n");
+	TRACE_DEBUG("================================\n");
+	TRACE_DEBUG("Copyright: 2010-2019 \n");
+	TRACE_DEBUG("Compiled: %s %s \n", __DATE__, __TIME__);
+	TRACE_DEBUG("Target: generic \n");
+	TRACE_DEBUG("\n");
+	TRACE_DEBUG("\n");
+
+	TRACE_DEBUG("1. Initializing communication with : %s \n", DIALOG_MULTISENSOR);
 
 	connection = gattlib_connect(NULL, DIALOG_MULTISENSOR, BDADDR_LE_PUBLIC, BT_SEC_SDP, 0, 0);
 	if (connection == NULL) {
-		fprintf(stderr, "Fail to connect to the bluetooth device.\n");
+		TRACE_ERROR("Fail to connect to the bluetooth device.\n");
 		return 1;
 	} else {
-	    fprintf(stderr, "Connected to %s \n", DIALOG_MULTISENSOR);
+		TRACE_DEBUG("Connected to %s \n", DIALOG_MULTISENSOR);
 	}
 
 /////////////////////////////////////
 
-	fprintf(stderr, "2. Reading : Device Features\n");
+	TRACE_DEBUG( "2. Reading : Device Features\n");
 
 	g_operation = READ;
 
@@ -107,11 +116,11 @@ int main(int argc, char *argv[]) {
 
 			gattlib_uuid_to_string(&g_uuid, uuid_str, sizeof(uuid_str));
 
-			fprintf(stderr, "Could not find GATT Characteristic with UUID %s\n", uuid_str);
+			TRACE_ERROR( "Could not find GATT Characteristic with UUID %s\n", uuid_str);
 			goto EXIT;
 		}
 
-		printf("Read UUID completed: ");
+		TRACE_DEBUG("Read UUID completed: ");
 		for (i = 0; i < len; i++) {
 			printf("%02x ", buffer[i]);
 		}
@@ -120,7 +129,7 @@ int main(int argc, char *argv[]) {
 
 /////////////////////////////////////
 
-	fprintf(stderr, "3. Writting: Control Point: Configuration\n");
+	TRACE_DEBUG( "3. Writting: Control Point: Configuration\n");
 
 	g_operation = WRITE;
 
@@ -137,13 +146,13 @@ int main(int argc, char *argv[]) {
 
 			gattlib_uuid_to_string(&g_uuid, uuid_str, sizeof(uuid_str));
 
-			fprintf(stderr, "Could not find GATT Characteristic with UUID %s\n", uuid_str);
+			TRACE_ERROR( "Could not find GATT Characteristic with UUID %s\n", uuid_str);
 			goto EXIT;
 		}
 	}
 
 	/////////////////////////////////////
-	   fprintf(stderr, "4. Writting: Control Point: Request\n");
+	TRACE_DEBUG( "4. Writting: Control Point: Request\n");
 
 	   g_operation = WRITE;
 
@@ -160,7 +169,7 @@ int main(int argc, char *argv[]) {
 
 				gattlib_uuid_to_string(&g_uuid, uuid_str, sizeof(uuid_str));
 
-				fprintf(stderr, "Could not find GATT Characteristic with UUID %s\n", uuid_str);
+				TRACE_ERROR( "Could not find GATT Characteristic with UUID %s\n", uuid_str);
 				goto EXIT;
 			}
 		}
@@ -197,7 +206,7 @@ int main(int argc, char *argv[]) {
 //				}
 
 /////////////////////////////////////
-		printf("6. Reading: Sensors Report\n");
+		TRACE_DEBUG("6. Reading: Sensors Report\n");
 
 		g_operation = READ;
 
@@ -216,19 +225,63 @@ int main(int argc, char *argv[]) {
 
 				gattlib_uuid_to_string(&g_uuid, uuid_str, sizeof(uuid_str));
 
-				fprintf(stderr, "Could not find GATT Characteristic with UUID %s\n", uuid_str);
+				TRACE_ERROR( "Could not find GATT Characteristic with UUID %s\n", uuid_str);
 				goto EXIT;
 			}
 
-			printf("Read UUID completed: ");
+			TRACE_DEBUG("Read UUID completed: ");
 			for (i = 0; i < len; i++) {
 				printf("%02x ", buffer[i]);
 			}
 			printf("\n");
 		}
 
+		//////////////////////////////////////////////////////////////////////////////////////////////////
+
+		value = buffer[5] + (buffer[6] << 8) + (buffer[7] << 16) + (buffer[8] << 24) ;
+		device.pressure = (float)(value)/100;
+
+		value = buffer[12] + (buffer[13] << 8) + (buffer[14] << 16) + (buffer[15] << 24) ;
+		device.humidity = (float)(value)/1000;
+
+        value = buffer[19] + (buffer[20] << 8) + (buffer[21] << 16) + (buffer[22] << 24) ;
+        device.temperature = (float)(value)/100;
+
+        TRACE_DEBUG("Temperature: %.2f \n", device.temperature);
+        TRACE_DEBUG("Humidity: %.2f \n", device.humidity);
+        TRACE_DEBUG("Pressure: %.2f \n", device.pressure);
+
+		{
+			FILE *f = fopen("ble_multisensor.txt", "w");
+			if (f == NULL) {
+				printf("Error opening file!\n");
+				exit(-1);
+			}
+
+			/* print some text */
+			fprintf(f, "temperature:%.2f\r\nhumidity:%.2f\r\npressure:%.2f\r\n",
+					device.temperature, device.humidity, device.pressure);
+			fclose(f);
+		}
+
+		{
+			FILE *f = fopen("ble_multisensor.raw", "w");
+
+			if (f == NULL) {
+				TRACE_DEBUG("Error opening file!\n");
+				exit(-1);
+			}
+			for (i = 0; i < len; i++) {
+				fprintf(f, "%02x ", buffer[i]);
+			}
+			fprintf(f, "\n");
+			fclose(f);
+		}
+
+
 EXIT:
 	gattlib_disconnect(connection);
+	TRACE_DEBUG("BLE Connection finished with status: %d \n", ret);
 	return ret;
 
 }
