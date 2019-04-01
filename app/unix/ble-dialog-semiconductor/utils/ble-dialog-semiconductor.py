c07af8052babea9556dbee85c757b026ece8001b
Omar Chavez <ochavez@esoftthings.com>
2019-04-01 10:59:37 +0200

    [BLE] sanity check

--- app/unix/ble-dialog-semiconductor/utils/ble-dialog-semiconductor.py
+++ app/unix/ble-dialog-semiconductor/utils/ble-dialog-semiconductor.py
@@ -26,8 +26,7 @@
 
 import pexpect
 import time
- 
-DEVICE = "80:EA:CA:70:A3:2B"
+
 
 def hexStrToInt(hexstr):
     val = int(hexstr[0:2],16) + (int(hexstr[3:5],16)<<8) + (int(hexstr[6:8],16)<<16) + (int(hexstr[9:11],16)<<24)
@@ -35,73 +34,85 @@ def hexStrToInt(hexstr):
         val = -((val^0xffff)+1)
         return val
 
-print("Multisensor Address:"),
-print(DEVICE)
- 
-# Run gatttool interactively.
-print("Run gatttool...")
-child = pexpect.spawn("gatttool -I")
- 
-# Connect to the device.
-print("Connecting to"),
-print(DEVICE),
-child.sendline("connect {0}".format(DEVICE))
-child.expect("Connection successful", timeout=5)
-print("(Connected)")
+class DialogSemiconductor(object):
 
+    def __init__(self, address):
+        self.address = address
+        self.tempetature = 0
+        self.pressure = 0
+        self.humidity = 0
+        self.child = 0
+        self.connect()
+        self.configure()
+        self.read()
+        self.disconnect()
+        pass
 
-# Device Info
-child.sendline("char-read-hnd 0x42")
-child.expect("Characteristic value/descriptor: ", timeout=5)
-child.expect("\r\n", timeout=5)
-print("Device Info: "),
-print(child.before),
- 
+    def connect(self):
+        # Connect to device.
+        self.child = pexpect.spawn("gatttool -I")
+        self.child.sendline("connect {0}".format(self.address))
+        self.child.expect("Connection successful", timeout=5)
 
-# request configuration  
-time.sleep(1)
-child.sendline("char-write-req 0x0044 0A080306030600020A000100000005")
-child.expect("Characteristic value was written successfully", timeout=5)
-child.expect("\r\n", timeout=5)
-print("Configuration Written: 0A080306030600020A000100000005"),
+        # Device Info
+        self.child.sendline("char-read-hnd 0x42")
+        self.child.expect("Characteristic value/descriptor: ", timeout=5)
+        self.child.expect("\r\n", timeout=5)
+        print("[BLE] Device Info: "+ self.child.before + "\n"),
 
-# save configuration into flash
-time.sleep(1)
-child.sendline("char-write-req 0x0044 04")
-child.expect("\r\n", timeout=5)
+    def configure(self):
+        # request configuration  
+        print("[BLE] Configuring: 0A080306030600020A000100000005\n"),
+        time.sleep(1)
+        self.child.sendline("char-write-req 0x0044 0A080306030600020A000100000005")
+        self.child.expect("Characteristic value was written successfully", timeout=5)
+        self.child.expect("\r\n", timeout=5)
 
-time.sleep(1)
-child.sendline("char-write-req 0x0044 01")
-child.expect("\r\n", timeout=5)
+        # save configuration into flash
+        self.child.sendline("char-write-req 0x0044 04")
+        self.child.expect("\r\n", timeout=5)
 
-# read status request
-time.sleep(1)
-child.sendline("char-read-hnd 0x0044")
-child.expect("Characteristic value/descriptor: ", timeout=5)
-child.expect("\r\n", timeout=5)
-print("\r\nConfiguration: "),
-print(child.before),
+        self.child.sendline("char-write-req 0x0044 01")
+        self.child.expect("\r\n", timeout=5)
 
+        # read status request
+        self.child.sendline("char-read-hnd 0x0044")
+        self.child.expect("Characteristic value/descriptor: ", timeout=5)
+        self.child.expect("\r\n", timeout=5)
 
-while True:	
-    time.sleep(1)
-    child.sendline("char-read-hnd 0x0049")
-    child.expect("Characteristic value/descriptor: ", timeout=5)
-    child.expect("\r\n", timeout=5)
-    print("\r\nMultisensor: "),
-    print(child.before),
+    def read(self):
+        time.sleep(1)
 
-    pressure = int(child.before[15:17], 16) + (int(child.before[18:20], 16) << 8) + (int(child.before[21:23], 16) << 16) + (int(child.before[24:26], 16) << 24)
-    print("\r\nPressure: ")
-    print(float(pressure) / 100)
-    
-    humidity = int(child.before[36:38], 16) + (int(child.before[39:41], 16) << 8) + (int(child.before[42:44], 16) << 16) + (int(child.before[45:47], 16) << 24)
-    print("\r\nHumidity: ")
-    print(float(humidity) / 1000)
-    
-    temperature = int(child.before[57:59], 16) + (int(child.before[60:62], 16) << 8) + (int(child.before[63:65], 16) << 16) + (int(child.before[66:68], 16) << 24)
-    print("\r\nTemperature: ")
-    print(float(temperature) / 100)
+        print("[BLE] Reading Device...")
+        self.child.sendline("char-read-hnd 0x0049")
+        self.child.expect("Characteristic value/descriptor: ", timeout=5)
+        self.child.expect("\r\n", timeout=5)
+        print("[BLE] Multisensor: " + self.child.before + "\n"),
 
+        file = open("/tmp/ble-dialog-semiconductor.txt","w")
+        file.write(self.child.before) 
+        file.close() 
 
+        self.pressure = int(self.child.before[15:17], 16) + (int(self.child.before[18:20], 16) << 8) + (int(self.child.before[21:23], 16) << 16) + (int(self.child.before[24:26], 16) << 24)
+        print("\r\nPressure: ")
+        print(float(self.pressure) / 100)
+        
+        self.humidity = int(self.child.before[36:38], 16) + (int(self.child.before[39:41], 16) << 8) + (int(self.child.before[42:44], 16) << 16) + (int(self.child.before[45:47], 16) << 24)
+        print("\r\nHumidity: ")
+        print(float(self.humidity) / 1000)
+        
+        self.temperature = int(self.child.before[57:59], 16) + (int(self.child.before[60:62], 16) << 8) + (int(self.child.before[63:65], 16) << 16) + (int(self.child.before[66:68], 16) << 24)
+        print("\r\nTemperature: ")
+        print(float(self.temperature) / 100)
+
+    def disconnect(self):
+        self.child.sendline("exit")
+
+###################################################################
+###################################################################
+###################################################################
+if __name__ == '__main__':
+    print("\n[BLE] Initializing BLE Application\n")
+    ble_device = DialogSemiconductor("80:EA:CA:70:A3:2B")
+    print("\n[BLE] Done\n")
 
